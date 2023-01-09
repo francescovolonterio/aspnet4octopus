dc126708b6a8a7c67cd1be8d0e52efbac7d2d20b
francesco.volonterio <francesco.volonterio@gmail.com>
2023-01-09 09:54:42 +0100

    initial commit for Jenkins

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace octopusTestAspNet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
